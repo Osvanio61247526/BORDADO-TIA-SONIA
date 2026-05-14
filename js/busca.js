3387de70a36544b9b60dd0173de32eb70875278e
Osvanio Jose de Souza <osvanio61247526@edu.df.senac.br>
2026-05-14 17:08:44 -0300

    trabalhando no busca.js

--- js/busca.js
+++ js/busca.js
@@ -3,3 +3,63 @@ const formBusca = document.querySelector('.busca');
 const campoBusca = document.querySelector('.busca__campo');
 const itensCatalogo = document.querySelectorAll('.bordados');
 
+  //verificar se o formulario de busca existe na página
+
+if(formBusca){
+    formBusca.addEventListener('submit', function(event){
+        event.preventDefault();
+    })   
+}
+    // agora vamos verificar o texto que foi digitado
+
+campoBusca.addEventListener('input', function(){
+    
+    //converter o texto para minúsculo para facilitar a comparação
+ const termoBusca = campoBusca.value.toLowerCase()
+
+   // vamos verificar todas as seções de bordados
+   itensCatalogo.forEach( function(item){
+
+    // pegar o elemento que tem o textodo bordado
+
+    const titulo = item.querySelector('h3').innerText.toLowerCase()  
+
+     //verificar se o termo de busca é igual ao título
+
+     if( titulo.includes(termoBusca)){
+        item.style.display = 'block'; // mostrar o item
+     }else{
+        item.style.display = 'none'; // ocultar o item
+     }
+
+   })
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
